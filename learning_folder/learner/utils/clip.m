903b49ca1b0a15f1f8ac4a16438f99475c7c3654
Agathe Balayn <agathe.balayn@ensta-paristech.fr>
2016-07-15 14:42:25 +0200

    removed old learning_folder

--- learning_folder/learner/utils/clip.m
+++
@@ -1,5 +0,0 @@
-function  y = clip( x, lo, hi )
- % ============> FAST VERSION <=============
- %  (uses matrix Logicals to replace Loops)
- y = (x .* [x<=hi])  +  (hi .* [x>hi]);
- y = (y .* [x>=lo])  +  (lo .* [x<lo]);
--- learning_folder/learner/utils/deg2rad.m
+++
@@ -1,7 +0,0 @@
-function [radians] = deg2rad(degrees)
-%DEG2RAD Summary of this function goes here
-%   Detailed explanation goes here
-
-radians = (pi/180) * degrees;
-
-end
